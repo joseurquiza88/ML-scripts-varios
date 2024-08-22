d68e77a513d93305bfda1e5a533fadff7e4dfd97
= <=>
2024-08-22 06:07:38 -0300

    NDVI code

--- 10_MERRA-2.R
+++ 10_MERRA-2.R
@@ -10,71 +10,139 @@
 # Extraccion de datos de MERRA-2
 rm(list=ls())
 
-punto <- cbind(lon = -70.659566, lat = -33.465694)  # Ejemplo con coordenadas de longitud y latitud
+coords <- data.frame(x = -70.659566, y = -33.465694) #01 ohg  # Ejemplo con coordenadas de longitud y latitud
+#coords <- data.frame(x = -70.66517052, y = -33.54606688) #02 BSQ
+#coords <- data.frame(x = -70.7503877, y = -33.43798487) #03 PDH
+# coords <- data.frame(x = -70.732100142, y = -33.43301075) #04 CNA
+#coords <- data.frame(x = -70.59475058, y = -33.59134682	) #05 PTA
+#coords <- data.frame(x = -70.732100142, y = -33.43301075) #06 FLD
+# coords <- data.frame(x = -70.52346222, y = -33.37639222) #07 CDE
+# coords <- data.frame(x = -70.74822755, y = -33.36576262) #08 QUI
 estacion <- "OHG"
+num_estacion <- "01"
+
+crs_project <- "+proj=longlat +datum=WGS84"
+punto <- SpatialPoints(coords, proj4string = CRS(crs_project))
+
+
+
 #Variables: DMSSMASS,DMSSMASS, DUSMASS, OCSMASS, SO2SMASS, SO4SMASS, y SSSMASS
-setwd("D:/Josefina/Proyectos/ProyectoChile/dataset/MERRA-2")
-ext = extent(-70.89626564382297,-70.54544288411844,-33.61652852593954,-33.33591657301113)#
-#raster_template <- raster(nrows = 86, ncols = 87,   crs = crs_project, ext =ext)
-crs_project <- "+proj=longlat +datum=WGS84"  # Ejemplo de CRS, ajusta según sea necesario
 
-# Calcular la resolución del píxel en grados
-# Asumiendo que las coordenadas están en grados, por ejemplo, si 1 grado = 111 km
-km_per_degree <- 111  # Aproximadamente 111 km por grado
-pixel_size_km <- 1  # Tamaño de píxel deseado en km
-
-# Calcular la resolución en grados
-resolution_degrees <- pixel_size_km / km_per_degree
-
-# Calcular el número de filas y columnas basados en la resolución
-nrows <- (ext@ymax - ext@ymin) / resolution_degrees
-ncols <- (ext@xmax - ext@xmin) / resolution_degrees
-
-# Redondear a números enteros para filas y columnas
-nrows <- ceiling(nrows)
-ncols <- ceiling(ncols)
-
-# Crear el raster con la extensión y resolución adecuada
-raster_template <- raster(nrows = nrows, ncols = ncols, crs = crs_project, ext = ext)
-
-
-dire <- "D:/Josefina/Proyectos/ProyectoChile/dataset/MERRA-2"
+dire <- "D:/Josefina/Proyectos/ProyectoChile/dataset/MERRA-2/2015/05-12-2015"
 setwd(dire)
 id <- list.files(path = getwd(),
                  pattern = "*.nc",
                  full.names = FALSE)
 
-archivo_nc<- "1MERRA2_400.tavg1_2d_aer_Nx.20150102.SUB.nc"
-# sds <- raster::stack(archivo_nc) 
-
-nameVar <- c("BCSMASS","DMSSMASS", "DUSMASS","DUSMASS25", "OCSMASS", "SO2SMASS", "SO4SMASS", "SSSMASS","SSSMASS25")
-
-df_rbind_2 <- data.frame()
-for (i in 1:length(id)){
-  print(i)
-  #print(paste("Esto es i = ", i, sep= ""))
-  archivo_nc = id[i]
-  df_rbind <- data.frame()
-  for (num_sds in 1:length(nameVar)){
-    name_sds<- nameVar[num_sds] #substring(sds[num_sds],31)
-    MIRRAraster <- raster(archivo_nc,varname=name_sds)
-    unit<- MIRRAraster@data@unit
-    date <- substr(archivo_nc,28,35)
-    crs_project <- "+proj=longlat +datum=WGS84"
-    MIRRAraster2 <- projectRaster(MIRRAraster,
-                                  crs = crs_project,
-                                  method = "bilinear")
-    rst_resampling <- raster::resample(MIRRAraster2, raster_template)
-  # Definir las coordenadas del punto
-    valor <- extract(rst_resampling, punto)
+for (i in 1:1){
+  ext = extent(-70.89626564382297,-70.54544288411844,-33.61652852593954,-33.33591657301113)#
+  #raster_template <- raster(nrows = 86, ncols = 87,   crs = crs_project, ext =ext)
+  # Ejemplo de CRS, ajusta según sea necesario
   
-  df <- data.frame (date= date,variable = name_sds, unidad=unit, value=valor,estacion=estacion)
+  # Calcular la resolución del píxel en grados
+  # Asumiendo que las coordenadas están en grados, por ejemplo, si 1 grado = 111 km
+  km_per_degree <- 111  # Aproximadamente 111 km por grado
+  pixel_size_km <- 1  # Tamaño de píxel deseado en km
   
-  df_rbind <- rbind(df_rbind,df)
+  # Calcular la resolución en grados
+  resolution_degrees <- pixel_size_km / km_per_degree
+  
+  # Calcular el número de filas y columnas basados en la resolución
+  nrows <- (ext@ymax - ext@ymin) / resolution_degrees
+  ncols <- (ext@xmax - ext@xmin) / resolution_degrees
+  
+  # Redondear a números enteros para filas y columnas
+  nrows <- ceiling(nrows)
+  ncols <- ceiling(ncols)
+  
+  # Crear el raster con la extensión y resolución adecuada
+  raster_template <- raster(nrows = nrows, ncols = ncols, crs = crs_project, ext = ext)
+  
+  
+  # archivo_nc<- "1MERRA2_400.tavg1_2d_aer_Nx.20150102.SUB.nc"
+  # sds <- raster::stack(archivo_nc) 
+  
+  nameVar <- c("BCSMASS","DMSSMASS", "DUSMASS","DUSMASS25", "OCSMASS", "SO2SMASS", "SO4SMASS", "SSSMASS","SSSMASS25")
+  
+  df_rbind_2 <- data.frame()
+  for (i in 1:length(id)){
+    print(i)
+    #print(paste("Esto es i = ", i, sep= ""))
+    archivo_nc = id[i]
+    df_rbind <- data.frame()
+    for (num_sds in 1:length(nameVar)){
+      name_sds<- nameVar[num_sds] #substring(sds[num_sds],31)
+      MIRRAraster <- raster(archivo_nc,varname=name_sds)
+      unit<- MIRRAraster@data@unit
+      date <- substr(archivo_nc,28,35)
+      crs_project <- "+proj=longlat +datum=WGS84"
+      MIRRAraster2 <- projectRaster(MIRRAraster,
+                                    crs = crs_project,
+                                    method = "bilinear")
+      rst_resampling <- raster::resample(MIRRAraster2, raster_template)
+      # Definir las coordenadas del punto
+      valor <- raster::extract(rst_resampling, punto)
+      
+      df <- data.frame (date= date,variable = name_sds, unidad=unit, value=valor,estacion=estacion)
+      
+      df_rbind <- rbind(df_rbind,df)
+    }
+    df_rbind_2<- rbind(df_rbind_2,df_rbind)
   }
-  df_rbind_2<- rbind(df_rbind_2,df_rbind)
 }
 View(df_rbind_2)
+
+# Generamos dataframe por separado y arreglamos los nombres
+for (i in 1:1){
+  BCSMASS <- df_rbind_2 [df_rbind_2$variable == "BCSMASS",]
+  BCSMASS <- data.frame(date=BCSMASS$date,unidad_BCSMASS=BCSMASS$unidad,BCSMASS=BCSMASS$value)
+  
+  
+  DMSSMASS <- df_rbind_2 [df_rbind_2$variable == "DMSSMASS",]
+  DMSSMASS <- data.frame(date=DMSSMASS$date,unidad_DMSSMASS=DMSSMASS$unidad,
+                         DMSSMASS=DMSSMASS$value)
+  
+  DUSMASS <- df_rbind_2 [df_rbind_2$variable == "DUSMASS",]
+  DUSMASS <- data.frame(date=DUSMASS$date,unidad_DUSMASS=DUSMASS$unidad,
+                        DUSMASS=DUSMASS$value)
+  
+  DUSMASS25 <- df_rbind_2 [df_rbind_2$variable == "DUSMASS25",]
+  DUSMASS25 <- data.frame(date=DUSMASS25$date,unidad_DUSMASS25=DUSMASS25$unidad,
+                          DUSMASS25=DUSMASS25$value)
+  
+  OCSMASS <- df_rbind_2 [df_rbind_2$variable == "OCSMASS",]
+  OCSMASS <- data.frame(date=OCSMASS$date,unidad_OCSMASS=OCSMASS$unidad,
+                        OCSMASS=OCSMASS$value)
+  
+  SO2SMASS <- df_rbind_2 [df_rbind_2$variable == "SO2SMASS",]
+  SO2SMASS <- data.frame(date=SO2SMASS$date,unidad_SO2SMASS=SO2SMASS$unidad,
+                         SO2SMASS=SO2SMASS$value)
+  
+  SO4SMASS <- df_rbind_2 [df_rbind_2$variable == "SO4SMASS",]
+  SO4SMASS <- data.frame(date=SO4SMASS$date,unidad_SO4SMASS=SO4SMASS$unidad,
+                         SO4SMASS=SO4SMASS$value)
+  
+  SSSMASS <- df_rbind_2 [df_rbind_2$variable == "SSSMASS",]
+  SSSMASS <- data.frame(date=SSSMASS$date,unidad_SSSMASS=SSSMASS$unidad,
+                        SSSMASS=SSSMASS$value)
+  
+  SSSMASS25 <- df_rbind_2 [df_rbind_2$variable == "SSSMASS25",]
+  SSSMASS25 <- data.frame(date=SSSMASS25$date,unidad_SSSMASS25=SSSMASS25$unidad,
+                          SSSMASS25=SSSMASS25$value)
+  
+  # Lista de dataframes
+  dataframes <- list(BCSMASS,DMSSMASS,DUSMASS,DUSMASS25,OCSMASS,
+                     SO2SMASS,SO4SMASS,SSSMASS,SSSMASS25)
+  
+  # Merge de todos los dataframes en la lista usando la columna 'date'
+  merged_df <- Reduce(function(x, y) merge(x, y, by = "date", all = TRUE), dataframes)
+  
+  
+  merged_df$estacion <- estacion
+}
+view(merged_df)
+
+write.csv(merged_df,paste("D:/Josefina/Proyectos/ProyectoChile/proceed/MERRA-2/2015/",num_estacion,"_",estacion,"_MERRA-2_01-04-2015.csv",sep=""))
 #######################################################################
 ########################################################################
 # Generar raster / tif para hacer el mapaa
---
+++ 11_NDVI.R
@@ -0,0 +1,78 @@
+
+# https://github.com/solrepresa/AQ-Valencia/blob/1732eab4542b64bb9429d7a89e9a4611b52acd12/src/AC_15.R#L7
+rm(list=ls())
+
+### Objetivo: Trabajar con NDVI de MOD13A3
+
+crs_project = "+proj=longlat +datum=WGS84 +no_defs +ellps=WGS84 +towgs84=0,0,0"
+
+# Uso imagen del STACK de MODIS MCD19A2 como modelo para crear raster
+# tomo el stack porq tiene el tamaño que me interesa conservar
+
+# MODIS <- raster("/home/usuario/Sol/aire_comunitat/stack/month/AOD_mes_01_max.tif")
+# MODIS <- st_read("D:/Josefina/Proyectos/ProyectoChile/shape/data_referencia/santiago_4326.shp")
+# 
+# raster_template <- raster(nrows = 239, ncols = 158, #100m de resolucion aprox
+#                           crs = crs_project, 
+#                           ext = extent(MODIS))  # toma las extensiones
+#coords <- data.frame(x = -70.659566, y = -33.465694) #01 ohg  # Ejemplo con coordenadas de longitud y latitud
+#coords <- data.frame(x = -70.66517052, y = -33.54606688) #02 BSQ
+coords <- data.frame(x = -70.7503877, y = -33.43798487) #03 PDH
+# coords <- data.frame(x = -70.732100142, y = -33.43301075) #04 CNA
+#coords <- data.frame(x = -70.59475058, y = -33.59134682	) #05 PTA
+#coords <- data.frame(x = -70.732100142, y = -33.43301075) #06 FLD
+# coords <- data.frame(x = -70.52346222, y = -33.37639222) #07 CDE
+# coords <- data.frame(x = -70.74822755, y = -33.36576262) #08 QUI
+estacion <- "PDH"
+num_estacion <- "03"
+
+
+punto <- SpatialPoints(coords, proj4string = CRS(crs_project))
+
+
+
+dir_raster = "D:/Josefina/Proyectos/ProyectoChile/dataset/NDVI/"
+setwd(dir_raster)
+
+id <- dir(dir_raster, pattern = ".hdf")
+df_rbind <- data.frame()
+for (i in 1:length(id)){
+  print(i)
+  file.name <- id[i]
+  sds <- get_subdatasets(file.name)
+  # Optical_Depth_055
+  gdal_translate(sds[1], dst_dataset = paste0('nvdi', basename(file.name), '.tiff'))
+  nvdi <- raster(paste0('nvdi', basename(file.name), '.tiff'))
+  crs_project <- "+proj=longlat +datum=WGS84"
+  # gdal_translate(sds[6], dst_dataset = paste0('qa', basename(file.name), '.tiff'))
+  # qa <- raster(paste0('qa', basename(file.name), '.tiff'))
+  SINU <- as.character(nvdi@crs)
+  proj4string(nvdi) <- CRS(SINU)
+  nvdi <- projectRaster(nvdi,crs = crs_project)
+  nvdi_factor <- nvdi*0.0001   #factor de escala 
+  
+  # Template
+  ext = extent(-70.89626564382297,-70.54544288411844,-33.61652852593954,-33.33591657301113)#
+  km_per_degree <- 111  # Aproximadamente 111 km por grado
+  pixel_size_km <- 1  # Tamaño de píxel deseado en km
+  resolution_degrees <- pixel_size_km / km_per_degree
+  nrows <- (ext@ymax - ext@ymin) / resolution_degrees
+  ncols <- (ext@xmax - ext@xmin) / resolution_degrees
+  nrows <- ceiling(nrows)
+  ncols <- ceiling(ncols)
+  raster_template <- raster(nrows = nrows, ncols = ncols, crs = crs_project, ext = ext)
+  
+  rst_resampling <- raster::resample(nvdi_factor, raster_template)
+  # Definir las coordenadas del punto
+  valor <- raster::extract(rst_resampling, punto)
+  date = substr(file.name,10,16)
+  df <- data.frame(date =date, ndvi=valor, estacion=estacion)
+  df_rbind <- rbind(df_rbind,df)
+  file.remove(dir('./', paste0('nvdi', basename(file.name), '*')))
+
+}
+
+View(df_rbind)
+write.csv(df_rbind,paste("D:/Josefina/Proyectos/ProyectoChile/dataset/proceed/NDVI/",num_estacion,"_",estacion,"_NDVI.csv",sep=""))
+
+
